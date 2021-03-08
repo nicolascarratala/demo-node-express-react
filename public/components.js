2b4042dcd736ae02af17778827a2607607bedf1d
nicolascarratala <nicocarratala@gmail.com>
2021-03-07 23:14:19 -0300

    ar

--- public/components.js
+++ public/components.js
@@ -33,7 +33,7 @@
         init: function () {
           var el = this.el;
           el.addEventListener('click', (e) => { 
-            this.el.setAttribute('position', "200 0 -200" )
+            this.el.setAttribute('position', "-470 0 -200" )
           });
         }
       });
