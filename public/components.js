ff8c9d5d5b371133defc6a8d24dc34cf7e74bb0a
nicolascarratala <nicocarratala@gmail.com>
2021-03-08 03:16:48 -0300

    ar

--- public/components.js
+++ public/components.js
@@ -39,14 +39,44 @@
             
             var pos = el.getAttribute('position').x;
 
-            // alert(el.getAttribute('position').x);
+            // a
             if(pos == 0){
               el.setAttribute('position', "-400 0 -200" );
             }
+            // b
             if(pos == -400){
               el.setAttribute('position', "-800 0 -200" );           
             }
+            // c
             if(pos == -800){
+              el.setAttribute('position', "-1200 0 -200" );
+            }
+            // d
+            if(pos == -1200){
+              el.setAttribute('position', "-1600 0 -200" );
+            }
+            // e
+            if(pos == -1600){
+              el.setAttribute('position', "-2000 0 -200" );
+            }
+            // f
+            if(pos == -2000){
+              el.setAttribute('position', "-2400 0 -200" );
+            }
+            // g
+            if(pos == -2400){
+              el.setAttribute('position', "-2800 0 -200" );
+            }
+            // h
+            if(pos == -2800){
+              el.setAttribute('position', "-3200 0 -200" );
+            }
+            // i
+            if(pos == -3200){
+              el.setAttribute('position', "-3600 0 -200" );
+            }
+            // j
+            if(pos == -3600){
               el.setAttribute('position', "0 0 -200" );
             }
           });
