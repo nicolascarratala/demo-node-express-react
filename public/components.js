21fb678a380344eb8b4afe4fb79250487263b4fa
nicolascarratala <nicocarratala@gmail.com>
2021-03-07 22:04:24 -0300

    ar

--- public/components.js
+++ public/components.js
@@ -28,3 +28,13 @@
           });
         }
       });
+
+      AFRAME.registerComponent('image-change', {
+        init: function () {
+          let src = ["https://walle-api-madam-birras.herokuapp.com/Cube_BaseColor.png","https://walle-api-madam-birras.herokuapp.com/Cube_BaseColor.png"]
+          var el = this.el;
+          el.addEventListener('click', (e) => { 
+            this.el.setAttribute('src', src[Math.floor(Math.random() * src.length)])
+          });
+        }
+      });
