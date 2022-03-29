f1e8089072cfce2eb27b4f5c63a6fcc05cf4e93c
Nicolás <nicocarratala@gmail.com>
2022-03-29 11:36:43 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { hot } from "react-hot-loader";
-import Home from './pages/Home';
-import SplashScreen from './pages/SplashScreen';
 
 const App = () => {
     return (
-        <div style={{backgroundColor: "#000"}}>
-            <SplashScreen/> 
+        <div >
+           Hola
         </div>
     );
 };
